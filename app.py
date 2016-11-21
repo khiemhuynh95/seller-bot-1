e95c80929b62e7f2e806562fd4b5e1ecdc191316
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-11-21 21:23:33 +0700

    just attachments

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def webhook():
 					try:  # someone sent us a message
 						sender_id = messaging_event["sender"]["id"]		# the facebook ID of the person sending you the message
 						recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-						message_text = messaging_event["message"]["text"]  # the message's text
+						#message_text = messaging_event["message"]["text"]  # the message's text
 
-						
+
 						message_attachments = messaging_event["message"]["attachments"]["payload"]["url"]
 						if (message_attachments != null):
 							log("IMAGE URL: " + message_attachments)
