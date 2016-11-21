3479a5e6d969a8026e5aac77170729fd8df2a767
khiemhuynh95 <khiemhuynh95@gmail.com>
2016-11-21 20:15:57 +0700

    add exception

--- app.py
+++ app.py
@@ -34,16 +34,18 @@ def webhook():
 		for entry in data["entry"]:
 			for messaging_event in entry["messaging"]:
 
-				if messaging_event.get("message"):  # someone sent us a message
-					sender_id = messaging_event["sender"]["id"]		# the facebook ID of the person sending you the message
-					recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-					message_text = messaging_event["message"]["text"]  # the message's text
+				if messaging_event.get("message"):
+					try:  # someone sent us a message
+						sender_id = messaging_event["sender"]["id"]		# the facebook ID of the person sending you the message
+						recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+						message_text = messaging_event["message"]["text"]  # the message's text
 
-					message_attachments = messaging_event["message"]["attachments"]["payload"]["url"]
-					log("IMAGE URL: " + message_attachments)
+						message_attachments = messaging_event["message"]["attachments"]["payload"]["url"]
+						log("IMAGE URL: " + message_attachments)
 
-					onMessageEvent(sender_id, recipient_id, message_text)
-
+						onMessageEvent(sender_id, recipient_id, message_text)
+					except:
+						pass
 				if messaging_event.get("delivery"):  # delivery confirmation
 					pass
 
